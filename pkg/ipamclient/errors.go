34c5e565883fe1ad553883ca33b1b52ac288ae63
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2023-06-27 09:12:57 -0400

    Fix error messages
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- pkg/ipamclient/errors.go
+++ pkg/ipamclient/errors.go
@@ -6,13 +6,13 @@ import (
 
 var (
 	// ErrIPAMHTTPUnauthorized is returned when the request is not authorized
-	ErrLBHTTPUnauthorized = errors.New("ipam api received unauthorized request")
+	ErrIPAMHTTPUnauthorized = errors.New("ipam api received unauthorized request")
 
 	// ErrIPAMHTTPNotfound is returned when the load balancer ID not found
-	ErrLBHTTPNotfound = errors.New("ipam ID not found")
+	ErrIPAMHTTPNotfound = errors.New("ipam ID not found")
 
 	// ErrIPAMHTTPError is returned when the http response is an error
-	ErrLBHTTPError = errors.New("ipam api http error")
+	ErrIPAMHTTPError = errors.New("ipam api http error")
 
 	// ErrNoAvailableIPs is returned when there are no available IPs within the specified block
 	ErrNoAvailableIPs = errors.New("no available IPS within specified block")
