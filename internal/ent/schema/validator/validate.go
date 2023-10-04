a94d0dbb49658a057d77248745575aad3378b8b6
Cherchezlafemme <18508181+cherchezlafemme@users.noreply.github.com>
2023-10-04 10:19:21 -0700

    Add method to validate IP Block Prefix (#58)
    Validate that a prefix is a valid IP prefix for IPBlocks.
    
    https://pkg.go.dev/net/netip#ParsePrefix
    
    Signed-off-by: Anya Krupp <akrupp@equinix.com>

--- internal/ent/schema/validator/validate.go
+++ internal/ent/schema/validator/validate.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"net/netip"
 )
 
 // IPAddr returns error if IP address is NOT valid
@@ -21,4 +22,22 @@ var ErrInvalidIPAddr = errors.New("provided IP Address is invalid")
 // InvalidIPAddrError returns Error Invalid IP Address
 func InvalidIPAddrError(ip string) error {
 	return fmt.Errorf("error %w: %s", ErrInvalidIPAddr, ip)
+}
+
+// IPBlockPref returns error if IP Block Prefix is NOT valid
+func IPBlockPref(prefix string) error {
+	_, err := netip.ParsePrefix(prefix)
+	if err != nil {
+		return InvalidIPPrefError(prefix)
+	}
+
+	return nil
+}
+
+// ErrInvalidIPPref is an error raised when provided IP Block Prefix is invalid
+var ErrInvalidIPPref = errors.New("provided IP Block Prefix is invalid")
+
+// InvalidIPPrefError returns Error Invalid IP Block Prefix
+func InvalidIPPrefError(prefix string) error {
+	return fmt.Errorf("error %w: %s", ErrInvalidIPPref, prefix)
 }
